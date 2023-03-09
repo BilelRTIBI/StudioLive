cdadd0e10692f8db4ea623cf841ca47242d81b72
Bilel Rtibi <bilel.rtibi@sastec-group.com>
2023-03-09 16:40:12 +0100

    commit 110

--- Tests/TS01-TC01-test-connexion.js
+++ Tests/TS01-TC01-test-connexion.js
@@ -14,7 +14,6 @@ module.exports = {
         .click('button[class=Settings__btnRejoindre]')
         .pause(5000)
         .waitForElementVisible('.Settings__err-msg')
-        //////////////////////////////////
         .pause(3000)
         .end()
 
